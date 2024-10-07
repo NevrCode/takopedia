eac9df25d7c35b25ade4d66ea4b7fc55d0b372fd
Testboiz <tvmini0202@gmail.com>
2024-10-07 21:57:24 +0700

    added payment_service firebase handler

--- lib/services/payment_service.dart
+++ lib/services/payment_service.dart
@@ -1,10 +1,33 @@
 import 'dart:developer';
-import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:takopedia/model/product_model.dart';
+import 'package:takopedia/model/sales_model.dart';
 
 class PaymentService {
-  // pe bikin
+  final CollectionReference _sales =
+      FirebaseFirestore.instance.collection('sales');
+
+  Future<void> addSales(SalesModel sales) async {
+    await _sales
+        .add({
+          'date': sales.salesDate,
+          'item': sales.product,
+          'price': sales.price,
+          'quantity': sales.quantity,
+        })
+        .then((value) => log("Product Added"))
+        .catchError((error) => log("Failed to add product: $error"));
+  }
+
+  Future<List<SalesModel>> fetchSales() async {
+    try {
+      QuerySnapshot snapshot = await _sales.get();
+      return snapshot.docs.map((doc) {
+        return SalesModel.fromMap(doc.data() as Map<String, dynamic>);
+      }).toList();
+    } catch (e) {
+      log("Error : $e");
+      return [];
+    }
+  }
 }
